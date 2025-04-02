6a6b9a678f39375b3bf048bf0d36e63c9805218f
Himanshi Gupta <himagupta2004@gmail.com>
2025-04-03 03:08:48 +0530

    added map

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express=require("express");
 const http=require("http");
 const app=express();
 const socketio=require("socket.io");
+const path=require("path");
 const server = http.createServer(app);
 const io= socketio(server);
 
 app.set("view engine","ejs");
 app.set(express.static(path.join(__dirname,"public")));
+app.use('/style', express.static(path.join(__dirname, 'public/style')));
 
+app.use('/javascripts', express.static(path.join(__dirname, 'public/javascripts')));
 io.on("connection",function(socket){
     console.log("connected to socket");
 })
 app.get("/",function(req,res){
-    res.send("connection established");
+    res.render("index");
 });
 
 server.listen(3000);
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,26 @@
 const socket=io(); //connection req jayegi
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+       const {latitude,longitude}= position.coords;
+        socket.emit("send-location",{latitude,longitude});
+    },(error)=>{
+        console.log(error);
+    },
+    {
+        enableHighAccuracy:true,
+        maximumAge:0,//ensure the fresh location data
+        timeout:2000,//in how much time it should check again
+        fallbackToIP: true,// Uses IP-based location if GPS fails 
+        allowBackgroundLocation: false,
+    }
+);
+}
+
+const map=L.map("map").setView([0,0],10); //set latitude longitude as 0 and zoom 10
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution:"OpenStrretMap"
+}).addTo(map);
+
+
